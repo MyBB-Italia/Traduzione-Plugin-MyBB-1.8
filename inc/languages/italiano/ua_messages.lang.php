081c2844cb53bbfe40d7c58dc1088e807230f5b3
chack1172 <NBerardozzi@gmail.com>
2019-01-03 16:13:51 +0100

    Aggiunto UA Messages
    Aggiunta la traduzione del plugin UA Messages alla raccolta

---
+++ inc/languages/italiano/ua_messages.lang.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * MyBB Copyright 2016 MyBB Group, All Rights Reserved
+ *
+ * Website: http://www.mybb.com
+ * License: http://www.mybb.com/about/license
+ *
+ */
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l["ua_messages_welcomeMessage"] = "Benvenuto a UA-Plugins";
+$l["ua_messages_beenQuoted_title"] = "Sei stato citato!";
+$l["ua_messages_beenQuoted_body"] = "{UA_USERNAME} ha citato un tuo messaggio nella discussione '{UA_TOPICTITLE}'";
+$l["ua_messages_beenTagged_title"] = "Sei stato taggato!";
+$l["ua_messages_beenTagged_body"] = "{UA_USERNAME} ti ha taggato nella discussione '{UA_TOPICTITLE}'";
+$l["ua_messages_beenReplied_title"] = "Risposta nella tua discussione!";
+$l["ua_messages_beenReplied_body"] = "{UA_USERNAME} ha risposto nella tua discussione '{UA_TOPICTITLE}'";
+$l["ua_messages_beenPMed_title"] = "Hai ricevuto un messaggio privato!";
+$l["ua_messages_beenPMed_body"] = "{UA_USERNAME} ti ha inviato un messaggio privato con oggetto: {UA_PM_TITLE}";
+$l["ua_messages_beenSubs_title"] = "Messaggio in una discussione sottoscritta";
+$l["ua_messages_beenSubs_body"] = "{UA_USERNAME} ha risposto nella discussione sottoscritta: {UA_PM_TITLE}";
+$l["ua_messages_unknown_title"] = "Hai ricevuto un messaggio";
+$l["ua_messages_unknown_body"] = "Hai ricevuto un/a messaggio/notifica. Sfortunatamente UA Messages non lo ha capito. Premi qui per andare al/la messaggio privato/discussione.";
+$l["ua_messages_browserUnsupported"] = "Questo forum utilizza il plugin UA Messages. Questo plugin ti invia notifiche desktop.\\n\\nPurtroppo il tuo browser non supporta questo plugin. Ti suggeriamo di aggiornare il tuo forum!";
+$l["ua_messages_requestTitle"] = "Mostrare messaggi?";
+$l["ua_messages_permissionRequest"] = "Ricevi un messaggio quando <br>- Sei stato quotato<br>- Qualcuno risponde in una tua discussione<br>- Vieni taggato (attraverso @tuonome)<br>- Ricevi un messaggio privato<br><br>Acconsenti ad inviarti i messaggi?";
+$l["ua_messages_permissionBlocked"] = "Sfortunanamente hai bloccato il plugin quando te lo abbiamo chiesto la prima volta. Il sito web (forum) non pu&ograve; chiedertelo di nuovo. Se vuoi attivarlo, devi andare nelle impostazioni del browser e cercare \"Alerts\" o \"Messages\". Qu&igrave; puoi modificare i permessi. Dopo averlo fatto devi ricaricare la pagina e automaticamente ti sar&agrave; richiesto.";
+$l["ua_messages_permissionNoNotif"] = "Non mostrare notifiche";
+$l["ua_messages_permissionRequestNotif"] = "Richiesta per le notifiche";
+$l["ua_messages_blockedCloseMessage"] = "Chiudi messaggio";
+$l["ua_messages_information"] = "Questo forum utilizza UA Messages il che significa che riceverai notifiche desktop se succeder&agrave; qualcosa sul forum.";
