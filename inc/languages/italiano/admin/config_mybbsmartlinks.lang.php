b6cf6918b906fb34bb67b10e619a8d25c2db3e6b
chack1172 <NBerardozzi@gmail.com>
2019-01-03 16:39:44 +0100

    Aggiunto MyBB-SmartLinks
    Aggiunta la traduzione del plugin MyBB-SmartLinks alla raccolta

---
+++ inc/languages/italiano/admin/config_mybbsmartlinks.lang.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * MyBB SmartLinks English Admin Language Pack
+ * Copyright 2017 MyBB Group, All Rights Reserved
+ *
+ */
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['mybbsmartlinks_info_name'] = "MyBB SmartLinks";
+$l['mybbsmartlinks_info_description'] = "Sostituisce delle specifiche parole neio messaggi con i link.";
+$l['mybbsmartlinks_uninstall'] = "MyBB SmartLinks - Disinstallazione";
+$l['mybbsmartlinks_uninstall_message'] = "Vuoi rimuovere tutti i dati del plugin dal database?";
+$l['mybbsmartlinks_can_manage_smartlinks'] = "Può gestire MyBB SmartLinks?";
+
+$l['smartlinks'] = "MyBB SmartLinks";
+$l['add_smartlink'] = "Aggiungi SmartLink";
+$l['add_smartlink_desc'] = "Qui puoi definire un nuovo smartlink.";
+$l['edit_smartlink'] = "Modifica SmartLink";
+$l['edit_smartlink_desc'] = "Qui puoi modificare la parola e il suo url.";
+$l['smartlink_filters'] = "MyBB SmartLinks";
+$l['smartlink_filters_desc'] = "Questa funzionalità ti permette di gestire una lista di parole che verranno sostituite automaticamente con l'url desiderato nei messaggi del forum.";
+
+$l['smartlink_id'] = "ID";
+$l['smartlink'] = "Parola";
+$l['smartlink_url'] = "URL";
+$l['smartlink_title'] = "Titolo URL";
+$l['smartlink_nofollow'] = "Nofollow";
+$l['smartlink_newtab'] = "Nuova Scheda";
+
+$l['smartlink_url_notitle'] = "no";
+$l['smartlink_edit_option'] = "Modifica SmartLink";
+$l['smartlink_delete_option'] = "Elimina SmartLink";
+
+$l['smartlink_desc'] = "Inserisci la parola a cui inserire il link.";
+$l['smartlink_max'] = "La parola non può essere lunga più di 100 caratteri.";
+$l['smartlink_url_desc'] = "Inserisci l'url che verrà aggiunto alla parola specificata.";
+$l['smartlink_url_invalid'] = "L'url inserito non è valido!";
+$l['smartlink_url_word_max'] = "La lunghezza dell'url non può essere più di 200 caratteri!";
+$l['smartlink_url_title_max'] = "Il titolo dell'url non può essere lungo più di 100 caratteri.";
+$l['smartlink_url_title_addtitle'] = "Inserire un titolo all'url?";
+$l['smartlink_url_title'] = "Titolo:";
+
+$l['smartlink_url_nofollow'] = "Aggiungere il nofollow agli url?";
+$l['smartlink_url_newtab'] = "Aprire gli url in una nuova scheda?";
+
+$l['error_smartlink_url_word_invalid'] = "L'url non può essere lo stesso della parola!";
+$l['error_smartlink_url_title_empty'] = "Non hai inserito un titolo all'url!";
+
+$l['save_smartlink'] = "Salva SmartLink";
+$l['no_smartlinks'] = "Non ci sono smartlink impostati al momento.";
+
+$l['error_missing_smartlink'] = "Non hai inserito una parola!";
+$l['error_invalid_slid'] = "Lo smartlink specificato non esiste.";
+$l['error_smartlink_filtered'] = "Lo smartlink inserito esiste già.";
+
+$l['success_added_smartlink'] = "Lo smartlink è stato aggiunto con successo.";
+$l['success_deleted_smartlink'] = "Lo smartlink è stato eliminato con successo.";
+$l['success_updated_smartlink'] = "Lo smartlink è stato aggiornato con successo.";
+
+$l['confirm_smartlink_deletion'] = "Sei sicuro di voler eliminare questo smartlink?";
+
+$l['admin_log_config_mybbsmartlinks_add'] = "Aggiunto SmartLink #{1} ({2} -> {3})";
+$l['admin_log_config_mybbsmartlinks_edit'] = "Modificato SmartLink #{1} ({2} -> {3})";
+$l['admin_log_config_mybbsmartlinks_delete'] = "Eliminato SmartLink #{1} ({2} -> {3})";
