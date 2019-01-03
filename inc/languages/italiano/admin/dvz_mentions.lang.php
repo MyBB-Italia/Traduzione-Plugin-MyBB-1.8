af1674a26082c392e8f519eae89377fce1830279
chack1172 <NBerardozzi@gmail.com>
2019-01-03 15:27:12 +0100

    Aggiunto DVZ Mentions
    Aggiunta la traduzione del plugin DVZ Mentions alla raccolta

---
+++ inc/languages/italiano/admin/dvz_mentions.lang.php
@@ -0,0 +1,47 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Modificata da: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['dvz_me_description'] = 'Converte <i>@username</i> nel link del profilo con il colore del gruppo. Integrazione con <i>MyAlerts 2.0.x</i>.';
+$l['dvz_me_alerts'] = '<br><br><b>Integrazione con MyAlerts:</b>';
+$l['dvz_me_alerts_install'] = 'installa';
+$l['dvz_me_alerts_uninstall'] = 'disinstalla';
+$l['dvz_me_alerts_installed'] = 'L\'integrazione con MyAlerts è stata installata.';
+$l['dvz_me_alerts_uninstalled'] = 'L\'integrazione con MyAlerts è stata disinstallata.';
+
+$l['dvz_mentions_admin_pluginlibrary_missing'] = 'Aggiungi <a href="https://mods.mybb.com/view/pluginlibrary">PluginLibrary</a> per poter utilizzare il plugin.';
+
+$l['setting_group_dvz_mentions'] = 'DVZ Mentions';
+$l['setting_group_dvz_mentions_desc'] = 'Impostazioni per DVZ Mentions.';
+
+$l['setting_dvz_mentions_keep_prefix'] = 'Mantenere il prefisso per menzionare un utente';
+$l['setting_dvz_mentions_keep_prefix_desc'] = 'Scegli se mostrare o no il prefisso "@" nei messaggi.';
+
+$l['setting_dvz_mentions_apply_username_style'] = 'Mostrare username stilizzati';
+$l['setting_dvz_mentions_apply_username_style_desc'] = 'Scegli se stilizzare l\'username a seconda del gruppo utente.';
+
+$l['setting_dvz_mentions_links_to_new_tabs'] = 'Aprire il profilo in una nuova scheda';
+$l['setting_dvz_mentions_links_to_new_tabs_desc'] = 'Scegli se il link del profilo deve essere aperto in una nuova scheda.';
+
+$l['setting_dvz_mentions_cs_collation'] = 'Collation case-sensitive degli username';
+$l['setting_dvz_mentions_cs_collation_desc'] = 'Scegli se il plugin deve eseguire transformazioni addizionali dei caratteri per rimanere compatibile con le collation case-sensitive della colonna <code>username</code>.';
+
+$l['setting_dvz_mentions_ignored_values'] = 'Username ignorati';
+$l['setting_dvz_mentions_ignored_values_desc'] = 'Inserisci i nomi utenti che non verranno menzionati in linee separate.';
+
+$l['setting_dvz_mentions_min_value_length'] = 'Lunghezza minima username';
+$l['setting_dvz_mentions_min_value_length_desc'] = 'Scegli la lunghezza minima degli username che dovranno essere menzionati.';
+
+$l['setting_dvz_mentions_max_value_length'] = 'Lunghezza massima username';
+$l['setting_dvz_mentions_max_value_length_desc'] = 'Scegli la lunghezza massima degli username che dovranno essere menzionati.';
+
+$l['setting_dvz_mentions_match_limit'] = 'Limite delle menzioni';
+$l['setting_dvz_mentions_match_limit_desc'] = 'Scegli il numero massimo di menzioni, in un unico messaggio, oltre il quale gli altri utenti non verranno menzionati.';
+
+$l['setting_dvz_mentions_query_limit'] = 'Limite query';
+$l['setting_dvz_mentions_query_limit_desc'] = 'Scegli il numero massimo di menzioni nella query al database (spesso una per pagina) oltre il quale gli altri utenti non verranno menzionati.';
