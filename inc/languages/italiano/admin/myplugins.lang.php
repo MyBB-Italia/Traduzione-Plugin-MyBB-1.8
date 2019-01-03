364874aa552319eb3cbfd7e1bfe2328914c53036
chack1172 <NBerardozzi@gmail.com>
2019-01-03 20:52:01 +0100

    Aggiunto MyPlugins
    Aggiunta la traduzione del plugin MyPlugins alla raccolta

---
+++ inc/languages/italiano/admin/myplugins.lang.php
@@ -0,0 +1,78 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['myplugins'] = "MyPlugins";
+
+//Admin Menu
+$l['myplugins_overview'] = "Panoramica";
+$l['myplugins_settings'] = "Impostazioni";
+$l['myplugins_template'] = "Modifiche template";
+$l['myplugins_stats'] = "Statistiche anonime";
+$l['myplugins_verification'] = "Verifica File";
+
+//Admin Permissions
+$l['myplugins_permission_list'] = "Può visualizzare i dettagli";
+$l['myplugins_permission_stats'] = "Può cambiare le impostazione delle statistiche";
+$l['myplugins_permission_verification'] = "Può usare la verifica dei file";
+
+$l['active_plugin'] = "Plugins Attivati";
+$l['no_active_plugins'] = "Non hai plugins attivi";
+$l['inactive_plugin'] = "Plugins Disattivati";
+$l['no_inactive_plugins'] = "Non hai plugins disattivati";
+$l['downloadable_plugin'] = "Download Plugins";
+$l['no_downloadable_plugins'] = "Non puoi scaricare nessun nuovo plugins";
+$l['no_connection'] = "Non posso connettermi al server";
+$l['md5'] = "MD5";
+$l['missing'] = "Mancante";
+$l['changed'] = "Modificato";
+
+$l['save'] = "Salva";
+$l['activate'] = "Attiva";
+$l['myplugins_activate'] = "Inviare statistiche anonime a JonesBoard?";
+$l['myplugins_activate_desc'] = "Saranno inviate solo le informazioni del server e quelle riguardanti i miei plugins.";
+$l['myplugins_code'] = "Codice statistiche";
+$l['myplugins_code_desc'] = "Questo codice è generato casualmente  ed è usato per gestire i dati.<br />
+Se inserirai questo Codice nel tuo <a href=\"http://jonesboard.de/usercp.php?action=myplugins\" target=\"_blank\">UCP</a> a JonesBoard, il tuo aiuto sarebbe migliore.";
+
+$l['update'] = "Aggiorna";
+$l['download'] = "Download";
+
+//Download
+$l['get_package'] = "Get needed package<br />";
+$l['invalid_file'] = "Nessuna corrispondenza del file trovata";
+$l['download_package1'] = "Non abbiamo trovato nessuna pacchetto per la tua lingua, verrà scaricato quello inglese<br />";
+$l['download_package2'] = "Il pacchetto inglese e il tuo pacchetto saranno scaricati<br />";
+$l['download_package3'] = "Il tuo pacchetto sarà scaricato<br />";
+$l['create_temp'] = "Crea una directory temporanea<br />";
+$l['create_temp_error'] = "Non è stato possibile creare la directory \"myplugins-temp\", per favore creala manualmente";
+$l['start_download'] = "Inizia il download di \"{1}\"<br />";
+$l['write_error'] = "Non è stato possibile aprire il file temporaneo";
+$l['file_saved'] = "File salvato correttamente<br />";
+$l['start_download_lang'] = "Inizia il download dei file della lingua<br />";
+$l['extract'] = "Estrazione .zip's...<br />";
+$l['extract_error'] = "Non è stato possibile estratte i(l) File .zip";
+$l['move'] = "Sposta tutti i file<br />";
+$l['move_error'] = "Non è stato possibile determinare la cartella";
+$l['delete_temp'] = "Prova a cancellare tutti i file temporanei<br />";
+$l['download_complete'] = "<b>Download completato</b>";
+$l['internal_error'] = "Errore interno";
+$l['move_to'] = "Sposta {1} -> {2}<br />";
+$l['update_note'] = "<br />Per favore segui le istruzione dell'aggiornamento";
+
+//Settings
+$l['error_chmod_settings_file'] = "Le impostazioni del file \"./inc/settings.php\" non sono riscrivibili. Per favore usa il CHMOD a 777.<br />Per maggiori informazioni sul CHMODing, guarda <a href=\"http://docs.mybb.com/HowTo_Chmod.html\" target=\"_blank\">MyBB Docs</a>.";
+$l['success_settings_updated'] = "Le impostazioni sono state aggiornate correttamente.";
+
+//Templates
+$l['template'] = "Template";
+$l['search'] = "Termine di ricerca";
+$l['replace'] = "Sostituzione";
+$l['added'] = "Aggiunto?";
+$l['template_missing'] = "Il template non esiste";
+$l['changed'] = "Cambiato";
+$l['not_changed'] = "Non è cambiato";
