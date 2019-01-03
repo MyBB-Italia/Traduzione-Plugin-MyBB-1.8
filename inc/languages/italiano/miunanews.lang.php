8e3f6fc2217df46c5101e2b5f6a6c6c67595ea15
chack1172 <NBerardozzi@gmail.com>
2019-01-03 15:44:00 +0100

    Aggiunto Miuna News
    Aggiunta la traduzione del plugin Miuna News alla raccolta

---
+++ inc/languages/italiano/miunanews.lang.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Miuna News
+ * https://github.com/martec
+ *
+ * Copyright (C) 2015-2015, Martec
+ *
+ * Miuna News is licensed under the GPL Version 3, 29 June 2007 license:
+ *	http://www.gnu.org/copyleft/gpl.html
+ *
+ * @fileoverview Miuna News - Websocket News for Mybb
+ * @author Martec
+ * @requires jQuery, Nodejs, Socket.io, Express, debug and Mybb
+ * @credits notify.png icon from http://community.mybb.com/post-861176.html
+ * @credits some part of code based in MyAlerts of Euan T (http://community.mybb.com/thread-127444.html)
+ */
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['miunanews_news'] = 'Notizie';
+$l['miunanews_recent_news'] = 'Notizie recenti';
+$l['miunanews_new_post'] = 'Nuovo messaggio';
+$l['miunanews_new_thread'] = 'Nuovo argomento';
+$l['miunanews_new_myalerts'] = 'Nuovo Myalerts';
+$l['miunanews_new_msg'] = 'Ci sono nuovi messaggi da ';
+$l['miunanews_new_msg2'] = ' in questo argomento.';
+$l['miunanews_new_myalerts_msg'] = 'Hai un nuovo avviso da myalerts.';
+$l['miunanews_has_more'] = 'Ci sono più messaggi da visualizzare. ';
+$l['miunanews_has_more2'] = 'Vuoi vederli?';
+$l['miunanews_spoiler'] = 'Spoiler';
+$l['miunanews_show'] = 'Mostra';
+$l['miunanews_hide'] = 'Nascondi';
+$l['miunanews_settings'] = 'Impostazioni delle notizie';
+$l['miunanews_old_news'] = 'Vuoi caricare notizie vecchie?';
+$l['miunanews_new_news'] = 'Vuoi caricare notizie nuove?';
+$l['miunanews_myalerts_news'] = 'Carica le notizie di Myalerts?';
+$l['miunanews_load_newpost'] = 'Carica i nuovi messaggi';
+$l['miunanews_tidign_newpost'] = 'Ignora l\'id di un argomento.<br /><small>Separa gli id con una virgola.</small>';
+$l['miunanews_no'] = 'No';
+$l['miunanews_yes'] = 'Si';
+$l['miunanews_update_config'] = 'Aggiorna le impostazioni delle notizie';
+$l['miunanews_update_saved'] = 'Le impostazioni delle notizie sono state salvate.';
