c558e91769f2f48625ae39a8821bb43d24a7534c
chack1172 <NBerardozzi@gmail.com>
2019-01-03 15:27:12 +0100

    Aggiunto DVZ Mentions
    Aggiunta la traduzione del plugin DVZ Mentions alla raccolta

---
+++ inc/languages/italiano/dvz_mentions_alerts_post.lang.php
@@ -0,0 +1,11 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Modificata da: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['myalerts_dvz_mentions_post_alert'] = '{1} ti ha menzionato in "{2}".';
+$l['myalerts_setting_dvz_mentions_post'] = 'Vuoi ricevere un avviso quando vieni menzionato in un post?';
