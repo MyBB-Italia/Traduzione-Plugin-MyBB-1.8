372f9cb8b8ed84737bb3bbe1e761f1a3693ef21c
chack1172 <NBerardozzi@gmail.com>
2019-01-03 15:48:36 +0100

    Aggiunto Mood Manager
    Aggiunta la traduzione del plugin Mood Manager alla raccolta

---
+++ inc/languages/italiano/admin/config_moods.lang.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Mood Manager
+ * Copyright 2012 Starpaul20
+ */
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Modificata da: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+ 
+$l['moodmanager_info_name'] = "Gestore Umore";
+$l['moodmanager_info_desc'] = "Permette agli utenti di impostare il loro umore da mostrare nel postbit/profilo.";
+
+$l['moods'] = "Umore";
+$l['can_manage_moods'] = "Può gestire gli umori?";
+
+$l['add_mood'] = "Aggiungi Nuovo Umore";
+$l['add_mood_desc'] = "Qui puoi aggiungere una singola nuova icona dell'umore.";
+$l['add_multiple_moods'] = "Aggiungi Umori Multipli";
+$l['add_multiple_moods_desc'] = "Qui puoi aggiungere nuovi umori multipli.";
+$l['edit_mood'] = "Modifica Umore";
+$l['edit_mood_desc'] = "Qui puoi modificare l'icona dell'umore.";
+$l['manage_moods'] = "Gestisci Umori";
+$l['manage_moods_desc'] = "Questa sezione ti permette di modificare, eliminare, e gestire le icone dell'umore.";
+
+$l['name_desc'] = "Questo è il nome dell'icona dell'umore. Usa <lang:language_string> per permettere la traduzione dei nomi degli umori (Le stringhe delle lingua devono essere aggiunte nel file mood.lang.php ).";
+$l['image_path'] = "Percorso dell'immagine";
+$l['image_path_desc'] = "Questo è il percorso dell'icona dell'immagine degli umori. Usa {lang} per rappresentare la lingua scelta dall'utente se la traduzione delle icone degli umori sono disponibili.";
+$l['save_mood'] = "Salva Umore";
+
+$l['name'] = "Nome";
+$l['image'] = "Immagine";
+$l['controls'] = "Controlli";
+$l['edit'] = "Modifica";
+$l['delete'] = "Elimina";
+
+$l['path_to_moods'] = "Percorso degli Umori";
+$l['path_to_moods_desc'] = "Questo è il percorso della cartella in cui ci sono le immagini degli umori.. <strong>Qualsiasi cartella con \"english\" nel suo percorso, verrà automaticamente cambiata con {lang} prima di essere aggiunta.</strong>";
+$l['show_moods'] = "Mostra Umori";
+$l['add'] = "Aggiungi?";
+$l['save_moods'] = "Salva Umori";
+
+$l['no_moods'] = "Al momento nel tuo forum non ci sono umori.";
+
+$l['error_missing_name'] = "Non hai inserito un nome per l'icona di questo messaggio";
+$l['error_missing_path'] = "Non hai inserito il percorso del messaggio di questa icona";
+$l['error_invalid_mood'] = "L'umore specificato non esiste.";
+$l['error_missing_path_multiple'] = "Non hai inserito un percorso";
+$l['error_invalid_path'] = "Non hai inserito un percorso valido";
+$l['error_no_mood_images'] = "Non ci sono immagini degli umori nella cartella specificata, oppure tutte le immagini presenti nella cartella sono state già aggiunte.";
+$l['error_none_included_moods'] = "Non hai selezionato nessun umore da includere.";
+
+$l['success_mood_added'] = "L'umore è stato aggiunto correttamente.";
+$l['success_moods_added'] = "Gli umori selezionati sono stati aggiunti correttamente.";
+$l['success_mood_updated'] = "L'umore è stato aggiornato correttamente.";
+$l['success_mood_deleted'] = "L'umore selezionato è stato cancellato correttamente.";
+
+$l['confirm_mood_deletion'] = "Sei sicuro di voler cancellare questo umore?";
+
+// Admin Log
+$l['admin_log_config_moods_add'] = "Aggiunto l'umore #{1} ({2})";
+$l['admin_log_config_moods_add_multiple'] = "Aggiunti umori multipli";
+$l['admin_log_config_moods_edit'] = "Modificato l'umore #{1} ({2})";
+$l['admin_log_config_moods_delete'] = "Cancellato l'umore #{1} ({2})";
