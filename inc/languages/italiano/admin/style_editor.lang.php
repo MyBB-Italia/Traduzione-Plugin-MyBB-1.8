f02e60c02db0a51a5962fc5e46c85065230e1c42
chack1172 <NBerardozzi@gmail.com>
2019-01-03 20:55:26 +0100

    Aggiunto Template Editor
    Aggiunta la traduzione del plugin Template Editor alla raccolta

---
+++ inc/languages/italiano/admin/style_editor.lang.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * MyBB Copyright 2016 MyBB Group, All Rights Reserved
+ *
+ * Website: http://www.mybb.com
+ * License: http://www.mybb.com/about/license
+ *
+ */
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['search'] = 'Cerca';
+$l['close'] = 'Chiudi';
+$l['templateEditor'] = 'Template Editor';
+$l['usedByAllThemes'] = 'Usato Da Tutti i Temi';
+$l['usedBy'] = 'Usato Da';
+$l['notUsedByAny'] = 'Non usato da nessun tema';
+$l['canUseTemplateEditor'] = 'Può usare template editor?';
+$l['unsavedChanges'] = 'Non hai salvato le modifiche in questa pagina. Vuoi uscire da questa pagina e annullare le modifiche o vuoi restare nella pagina?';
+$l['saveAndContinueEditing'] = 'Salva e Continua a Modificare';
+$l['couldNotSave'] = 'Impossibile salvare';
+$l['savedTheTemplate'] = 'Template salvato!';
