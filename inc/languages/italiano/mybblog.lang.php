e8f8fa36b151107f539648f3f4c7a180f4a88307
chack1172 <NBerardozzi@gmail.com>
2019-01-03 15:55:44 +0100

    Aggiunto MyBBlog
    Aggiunta la traduzione del plugin MyBBlog alla raccolta

---
+++ inc/languages/italiano/mybblog.lang.php
@@ -0,0 +1,64 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['mybblog'] = "Blog";
+$l['mybblog_deactivated'] = "MyBBlog non è ancora attivato";
+
+$l['mybblog_write'] = "Scrivi un nuovo articolo";
+$l['edit'] = "Modifica";
+
+$l['mybblog_articles_none'] = "Nessun articolo è stato trovato";
+$l['mybblog_posted'] = "Hai postato {1} da {2}";
+$l['mybblog_comments'] = "{1} Commenti";
+$l['mybblog_tags'] = "Taggato con {1}";
+
+$l['mybblog_new_article'] = "Scrivi nuovo articolo";
+$l['mybblog_edit_article'] = "Modifica articolo";
+$l['mybblog_title'] = "Titolo";
+$l['mybblog_article_tags'] = "Tag";
+$l['mybblog_tags_desc'] = "Separato da una virgola";
+$l['mybblog_article'] = "Articolo";
+$l['mybblog_article_submit'] = "Salva articolo";
+$l['mybblog_article_written'] = "L'articolo è stato salvato!";
+
+$l['mybblog_article_comments'] = "Commenti";
+
+$l['mybblog_new_comment'] = "Scrivi un nuovo commento";
+$l['mybblog_edit_comment'] = "Modifica commento";
+$l['mybblog_comment_submit'] = "Salva commento";
+$l['mybblog_comment_saved'] = "Il commento è stato salvato!";
+
+$l['mybblog_article_delete'] = "Vuoi eliminare 'articolo \"{1}\" e tutti i suoi dati associati?";
+$l['mybblog_comment_delete'] = "Vuoi eliminare il commento su un articolo \"{1}\"?";
+$l['mybblog_delete_confirm'] = "Sei certo di voler eliminare i dati selezionati?";
+$l['mybblog_deleted'] = "Elimina i dati selezionati";
+
+$l['mybblog_article_no_title'] = "Non hai inserito un titolo valido";
+$l['mybblog_article_no_content'] = "Non hai inserito un articolo";
+$l['mybblog_article_no_tags'] = "Hai bisogno di almeno un tag";
+$l['mybblog_article_unknown'] = "C'era un errore sconosciuto mentre stavi salvando il tuo articolo";
+
+$l['mybblog_invalid_article'] = "L'articolo che hai specificato non esiste";
+$l['mybblog_invalid_comment'] = "Il commento che hai specificato non esiste";
+
+$l['mybblog_comment_no_content'] = "Non hai inserito un qualsiasi commento";
+
+$l['mybblog_tag_no_tag'] = "C'è stato un tag vuoto";
+
+$l['myalerts_setting_JB_mybblog_new_comment'] = "Vuoi ricevere un avviso per nuovi commenti sui tuoi post del blog?";
+$l['myalerts_format_mybblog_new_comment'] = "{1} ha commentato sul tuo post del blog <a href=\"{3}\">{4}</a> ({2})";
+
+$l['mybblog_index'] = "Visualizza <a href=\"mybblog.php\">il blog</a>";
+$l['mybblog_writing'] = "Scrivere un nuovo articolo";
+$l['mybblog_view'] = "Leggi l'articolo <a href=\"{1}\">{2}</a>";
+$l['mybblog_tag'] = "Visualizzare gli articoli taggati con <a href=\"{1}\">{2}</a>";
+$l['mybblog_comment'] = "Commentare un articolo <a href=\"{1}\">{2}</a>";
+$l['mybblog_edit'] = "Modificare l'articolo <a href=\"{1}\">{2}</a>";
+$l['mybblog_delete'] = "Eliminazione di un articolo";
+$l['mybblog_delete_comment'] = "Eliminazione di un commento";
+$l['mybblog_edit_comment'] = "Modifica di un commento";
