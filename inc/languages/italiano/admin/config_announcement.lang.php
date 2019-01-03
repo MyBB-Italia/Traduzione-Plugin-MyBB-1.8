cbe6923e713f26c222f83cc3d6bc7f5c56df7996
chack1172 <NBerardozzi@gmail.com>
2019-01-03 16:35:14 +0100

    Aggiunto Announcement
    Aggiunta la traduzione del plugin Announcement alla raccolta

---
+++ inc/languages/italiano/admin/config_announcement.lang.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * MyBB Copyright 2016 MyBB Group, All Rights Reserved
+ *
+ * Website: http://www.mybb.com
+ * License: http://www.mybb.com/about/license
+ *
+ */
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['announcement'] = "Annunci";
+$l['order'] = "Ordine";
+$l['announcement_simple'] = "Annuncio";
+$l['announcement_error'] = "Errore sconosciuto. Riprova pi&ugrave; tardi.";
+$l['announcement_permission'] = "Pu&ograve; gestire gli annunci?";
+$l['announcement_no'] = "Non ci sono annunci";
+
+$l['left'] = "Sinistra";
+$l['right'] = "Destra";
+$l['top'] = "Sopra";
+$l['bottom'] = "Sotto";
+
+$l['scroll_none'] = "Senza scorrimento";
+$l['scroll_right'] = "Sinistra -> Destra";
+$l['scroll_left'] = "Destra -> Sinistra";
+$l['scroll_both'] = "Avanti e indietro";
+
+$l['announcement_list'] = "Annunci";
+$l['announcement_list_desc'] = "Qui puoi gestire gli annunci";
+$l['announcement_add'] = "Aggiugi Annuncio";
+$l['announcement_add_desc'] = "Qui puoi aggiungere un nuovo annuncio";
+$l['announcement_add_success'] = "Annuncio aggiunto con successo";
+$l['announcement_enable_success'] = "Annuncio {1} con successo";
+$l['announcement_delete_success'] = "Annuncio eliminato con successo";
+$l['announcement_edit_success'] = "Annuncio modificato con successo";
+$l['announcement_order_success'] = "Ordine cambiato";
+
+$l['announcement_deactivate'] = "disattivato";
+$l['announcement_activate'] = "attivato";
+
+$l['announcement_location'] = "Globale?";
+
+$l['announcement_submit'] = "Salva";
+$l['announcement_desc'] = "Il messaggio che verr&agrave; mostrato";
+$l['announcement_not'] = "Non &egrave; stato inserito alcun messaggio";
+$l['announcement_global'] = "&Egrave; un annuncio globale?";
+$l['announcement_global_not'] = "Errore Interno [1]";
+$l['announcement_forum'] = "In quali forum deve essere mostrato l'annuncio?";
+$l['announcement_forum_desc'] = "Se non scegli nulla e imposti nessuna discussione l'annuncio verr&agrave; mostrato nella index";
+$l['announcement_thread'] = "In quali discussioni deve essere mostrato l'annuncio?";
+$l['announcement_thread_desc'] = "Separa le discussioni con \",\"<br />Se non scegli nulla e non scegli alcun forum l'annuncio verr&agrave; mostrato nella index";
+$l['announcement_group'] = "A quali gruppi utente mostrare l'annuncio?";
+$l['announcement_group_desc'] = "Se non scegli nulla l'annuncio verr&agrave; visualizzato da tutti";
+$l['announcement_languages'] = "In che lingua &egrave; scritto l'annuncio?";
+$l['announcement_languages_desc'] = "Gli utenti con un'altra lingua non possono visualizzare l'annuncio.<br />Se non scegli nulla l'annuncio verr&agrave; visualizzato da tutti";
+$l['announcement_color'] = "Colore Testo";
+$l['announcement_color_desc'] = "Inserisci il colore del testo dell'annuncio";
+$l['announcement_color_not'] = "Si prega di inserire un colore per il testo";
+$l['announcement_back_color'] = "Colore Sfondo";
+$l['announcement_back_color_desc'] = "Inserisci il colore di sfondo dell'annuncio";
+$l['announcement_back_color_not'] = "Si prega di inserire un colore di sfondo";
+$l['announcement_border_select'] = "Bordo";
+$l['announcement_border_select_desc'] = "Dove mostrare un bordo?";
+$l['announcement_border_select_not'] = "Errore Interno [2]";
+$l['announcement_border_color'] = "Colore Bordo";
+$l['announcement_border_color_desc'] = "Il colore del bordo dell'annuncio";
+$l['announcement_border_color_not'] = "Si prega di inserire un colore al bordo";
+$l['announcement_scroll'] = "Scorrimento Testo";
+$l['announcement_scroll_desc'] = "";
+$l['announcement_scroll_not'] = "Errore Interno [3]";
+$l['announcement_slow_down'] = "Rallenta Scorrimento";
+$l['announcement_slow_down_desc'] = "Rallentare lo scorrimento quando l'utente sposta il mouse sull'annuncio?";
+$l['announcement_slow_down_not'] = "Errore Interno [4]";
+$l['announcement_css'] = "CSS aggiuntivo";
+$l['announcement_css_desc'] = "Qui puoi aggiungere del CSS agggiuntivo all'annuncio";
+$l['announcement_removable'] = "Nascondibile?";
+$l['announcement_removable_desc'] = "Gli utenti possono nascondere questo annuncio? (come gli annunci dei MP)?";
+$l['announcement_removable_not'] = "Errore Interno [5]";
+$l['announcement_enable'] = "Attivare Annuncio?";
+$l['announcement_enable_not'] = "Errore Interno [6]";
