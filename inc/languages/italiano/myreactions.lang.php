663eb32c8b50149770f9bc324260c9887de859b0
chack1172 <NBerardozzi@gmail.com>
2019-01-03 16:07:19 +0100

    Aggiunto MyReactions
    Aggiunta la traduzione del plugin MyReactions alla raccolta

---
+++ inc/languages/italiano/myreactions.lang.php
@@ -0,0 +1,38 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: MyBB Italia
+// Plugin: MyReactions
+// --------------------------------------------------------------------------------
+
+/**
+ * MyReactions 0.0.2 - Language File
+
+ * Copyright 2016 Matthew Rogowski
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ ** http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**/
+
+$l['myreactions_add'] = 'Aggiungi Reazione';
+$l['myreactions_remove'] = 'Rimuovi';
+$l['myreactions_all'] = 'Tutte le Reazioni';
+$l['myreactions_favourites'] = 'Reazione Preferite';
+$l['myreactions_you_reacted_with'] = 'Hai reagito con:';
+$l['myreactions_error_own_post'] = 'Non puoi reagire ai tuoi messaggi';
+$l['myreactions_no_multiple'] = 'Hai già reagito a questo messaggio';
+$l['myreactions_already_reacted'] = 'Hai già reagito a questo messaggio con questa reazione';
+$l['myreactions_profile_header'] = 'MyReactions';
+$l['myreactions_profile_received'] = 'Top Reazioni Ricevute';
+$l['myreactions_profile_given'] = 'Top Reazioni Date';
+$l['myreactions_profile_none'] = 'Nessuna reazione da mostrare.';
