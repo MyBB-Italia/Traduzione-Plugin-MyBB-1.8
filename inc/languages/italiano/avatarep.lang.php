d1b5eda137a7db0fa892d89b2cf6782530d7e13d
chack1172 <NBerardozzi@gmail.com>
2019-01-03 13:25:37 +0100

    Aggiunto Last Poster Avatar
    Aggiunta la traduzione del plugin Last Poster Avatar alla raccolta

---
+++ inc/languages/italiano/avatarep.lang.php
@@ -0,0 +1,19 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Modificata da: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+$l['avatarep_user_profile'] = "Vedi Profilo";
+$l['avatarep_user_messages'] = "Vedi Messaggi";
+$l['avatarep_user_sendpm'] = "Invia PM";
+$l['avatarep_user_sendemail'] = "Invia E-mail";
+$l['avatarep_user_threads'] = "Vedi Discussioni";
+$l['avatarep_user_error'] = " Errore ";
+$l['avatarep_user_error_text'] = " Devi eseguire l'accesso per vedere questo contenuto ";
+$l['avatarep_user_alt'] = "Avatar di {1}";
+$l['avatarep_user_no_avatar'] = "Nessun avatar";
+$l['avatarep_retrieving'] = "Recuperando Informazioni";
+$l['avatarep_loading'] = "Caricamento...";
