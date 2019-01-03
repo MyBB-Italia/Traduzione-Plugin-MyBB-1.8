82794cc88980aa129c79ca1fb675c4143460ccbe
chack1172 <NBerardozzi@gmail.com>
2019-01-03 16:10:02 +0100

    Aggiunto ProStats
    Aggiunta la traduzione del plugin ProStats alla raccolta

---
+++ inc/languages/italiano/prostats.lang.php
@@ -0,0 +1,39 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['prostats_prostats'] = 'Statistiche del forum';
+$l['prostats_reload'] = 'Ricarica';
+
+$l['prostats_username'] = 'Username';
+$l['prostats_posts'] = 'Messaggi';
+$l['prostats_datetime'] = 'Data, ora';
+$l['prostats_forum'] = 'Forum';
+
+$l['prostats_latest_posts'] = 'Ultimi messaggi';
+$l['prostats_most_replies'] = 'I più attivi';
+$l['prostats_most_reputations'] = 'Maggiore reputazione';
+$l['prostats_most_thanks'] = 'Maggiori ringraziamenti';
+$l['prostats_most_views'] = 'I più visti';
+$l['prostats_newest_members'] = 'Nuovi membri';
+$l['prostats_top_downloads'] = 'Top downloads';
+$l['prostats_top_posters'] = 'Top messaggi';
+$l['prostats_top_thread_posters'] = 'Top creatori discussioni';
+$l['prostats_top_topreferrers'] = 'Top referenti';
+
+$l['prostats_topic'] = 'Argomento';
+$l['prostats_author'] = 'Autore';
+$l['prostats_last_sender'] = 'Ultimo mittente';
+$l['prostats_replies'] = 'Risposte';
+
+$l['prostats_views'] = 'Visite';
+$l['prostats_subject'] = 'Titolo';
+
+$l['prostats_err_thxplugin'] = 'Per favore installa il plugin dei:<a href="http://prostats.wordpress.com/support/">grazie./a>';
+$l['prostats_err_undefind'] = 'Non definito';
+
+$l['prostats_disabled'] = 'Non puoi usare le funzionalità di prostats poichè sono state disabilitate dall\'aministratore.';
