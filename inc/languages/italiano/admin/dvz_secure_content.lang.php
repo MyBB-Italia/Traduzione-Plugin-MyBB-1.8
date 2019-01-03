9013f0903589075e644b2091ba031513da118d5a
chack1172 <NBerardozzi@gmail.com>
2019-01-03 15:35:59 +0100

    Aggiunto DVZ Secure Content
    Aggiunta la traduzione del plugin DVZ Secure Content alla raccolta

---
+++ inc/languages/italiano/admin/dvz_secure_content.lang.php
@@ -0,0 +1,34 @@
+<?php
+/* Translated into italian language by Riccardo Vianello; Copyright (C) 2015
+ released under Creative Commons BY-NC-SA 4.0 license: http://creativecommons.org/licenses/by-nc-sa/4.0/ */
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['setting_group_dvz_secure_content'] = 'DVZ Protezione del contenuto';
+$l['setting_group_dvz_secure_content_desc'] = 'Impostazioni per la Protezione dei contenuti di DVZ.';
+
+$l['setting_dvz_sc_images'] = 'Filtra le immagini di MyCode non HTTPS';
+$l['setting_dvz_sc_images_desc'] = 'Impedisci la visualizzazione di immagini di myCode non HTTPS sostituendoli con i link.';
+
+$l['setting_dvz_sc_avatars'] = 'Blocca gli avatar non HTTPS';
+$l['setting_dvz_sc_avatars_desc'] = 'Richiedi avatar remoti ad essere collegati su HTTPS.';
+
+$l['setting_dvz_sc_gravatar'] = 'Immagini HTTPS Gravatar';
+$l['setting_dvz_sc_gravatar_desc'] = 'Salva le e-mail basate sulle immagini di Gravatar utilizzando HTTPS (non quelle predefinite di MyBB).';
+
+$l['dvz_sc_embed_templates'] = 'Proteggi i template video di MyCode';
+$l['dvz_sc_embed_templates_message'] = 'I template video di MyCode incorporati sono stati modificati con l\'uso di HTTPS (links solo se non è disponibile).';
+$l['dvz_sc_embed_templates_revert'] = 'Ripristina i cambiamenti ai template video di MyCode';
+$l['dvz_sc_embed_templates_revert_message'] = 'I cambiamenti ai template video di MyCode sono state ripristinate.';
+$l['dvz_sc_replace_gravatar'] = 'Proteggia gli avatar di Gravatar';
+$l['dvz_sc_replace_gravatar_message'] = 'Gli avatar di Gravatar sono stati modificati per l\'uso in HTTPS.';
+$l['dvz_sc_remove_avatars'] = 'Rimuovi gli avatar remoti non HTTPS';
+$l['dvz_sc_remove_avatars_message'] = 'Gli avatar remoti Non HTTPS sono stati rimossi.';
+
+$l['dvz_sc_secure_avatars'] = 'Tutti gli avatar remoti stanno utilizzando il protocollo HTTPS';
+$l['dvz_sc_secure_embed_templates'] = 'Proteggi l\'incorporamento dei video di Mycode';
