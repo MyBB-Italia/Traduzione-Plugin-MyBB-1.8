51bc2774fd2558e0401bd8d34de539b090547631
chack1172 <NBerardozzi@gmail.com>
2019-01-03 15:39:21 +0100

    Aggiunto DVZ Shoutbox
    Aggiunta la traduzione del plugin DVZ Shoutbox alla raccolta

---
+++ inc/languages/italiano/admin/dvz_shoutbox.lang.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * MyBB Copyright 2016 MyBB Group, All Rights Reserved
+ *
+ * Website: http://www.mybb.com
+ * License: http://www.mybb.com/about/license
+ *
+ */
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['setting_dvz_sb_num'] = 'Messaggi da mostrare';
+$l['setting_dvz_sb_num_desc'] = 'Numero di messaggi da mostrare nella chat.';
+$l['setting_dvz_sb_num_archive'] = 'Messaggio da mostrare nell\'archivio';
+$l['setting_dvz_sb_num_archive_desc'] = 'Numero di messaggi da mostrare in ogni pagina nell\'archivio.';
+$l['setting_dvz_sb_reversed'] = 'Ordine inverso';
+$l['setting_dvz_sb_reversed_desc'] = 'Inverte l\'ordine di visualizzazione dei messaggi nella chat in modo che i nuovi messaggi appaiano in fondo. Si consiglia inoltre di spostare la variabile <b>{$panel}</b> variable sotto la finesta (div con classe window) nel template globale <i>dvz_shoutbox</i>.';
+$l['setting_dvz_sb_height'] = 'Altezza chat';
+$l['setting_dvz_sb_height_desc'] = 'L\'altezza della chat in pexel.';
+$l['setting_dvz_sb_dateformat'] = 'Formato Data';
+$l['setting_dvz_sb_dateformat_desc'] = 'Formato in cui vieni visualizzata la data. Questo formato utilizza la <a href="https://secure.php.net/manual/it/function.date.php#refsect1-function.date-parameters">sintassi della funzione date()</a> di PHP.';
+$l['setting_dvz_sb_maxlength'] = 'Lunghezza massima messaggio';
+$l['setting_dvz_sb_maxlength_desc'] = 'Imposta a 0 per rimuovere il limite.';
+$l['setting_dvz_sb_mycode'] = 'Convertire MyCode';
+$l['setting_dvz_sb_mycode_desc'] = '';
+$l['setting_dvz_sb_smilies'] = 'Convertire smilies';
+$l['setting_dvz_sb_smilies_desc'] = '';
+$l['setting_dvz_sb_interval'] = 'Intervallo Aggiornamento';
+$l['setting_dvz_sb_interval_desc'] = 'Numero di secondi da aspettare per aggiornare la chat (valori bassi migliorano la sincronizzazione ma causa un alto livello di caricamento del server). Imposta a 0 per disattivare l\'aggiornamento della chat.';
+$l['setting_dvz_sb_away'] = 'Chat Minimizzata';
+$l['setting_dvz_sb_away_desc'] = 'Numero di secondi dopo l\'ultima azione dell\'utente (es. click) dopo i quali la chat verr&agrave; minimizzata per prevenire un utilizzo non necessario delle risorse del server. Imposta a 0 per disattivare questa funzione.';
+$l['setting_dvz_sb_antiflood'] = 'Intervallo Anti-flood';
+$l['setting_dvz_sb_antiflood_desc'] = 'Numero di secondi che devo aspettare l\'utente prima di poter inviare un nuovo messaggio (non &egrave; applicato ai moderatori della chat).';
+$l['setting_dvz_sb_sync'] = 'Sincronizzazione moderazione';
+$l['setting_dvz_sb_sync_desc'] = 'Esegue le azioni di moderazione senza ricaricare la chat.';
+$l['setting_dvz_sb_mark_unread'] = 'Contrassegna messaggi non letti';
+$l['setting_dvz_sb_mark_unread_desc'] = 'Contrassegna i messaggi apparsi dopo l\'ultima visita dell\'utente.';
+$l['setting_dvz_sb_lazyload'] = 'Caricamento pigro';
+$l['setting_dvz_sb_lazyload_desc'] = 'Comincia a caricare i dati solo quando la chat &egrave; mostrata nello schermo (la pagina viene fatta scorrere fino alla posizione della chat).';
+$l['setting_dvz_sb_status'] = 'Stato di default chat';
+$l['setting_dvz_sb_status_desc'] = 'Scegli se la chat deve essere mostrata o nascosta di default.';
+$l['setting_dvz_sb_minposts'] = 'Minimo messaggi richiesti per inviare un messaggio nella chat';
+$l['setting_dvz_sb_minposts_desc'] = 'Imposta a 0 per farla utilizzare a tutti.';
+$l['setting_dvz_sb_groups_view'] = 'Permessi gruppo: Visualizzazione';
+$l['setting_dvz_sb_groups_view_desc'] = 'Gruppi utente che possono vedere la chat.';
+$l['setting_dvz_sb_groups_refresh'] = 'Permessi gruppo: Aggiornamento';
+$l['setting_dvz_sb_groups_refresh_desc'] = 'Gruppi utente a cui far ricaricare automaticamente la chat.';
+$l['setting_dvz_sb_groups_shout'] = 'Permessi gruppo: Invio';
+$l['setting_dvz_sb_groups_shout_desc'] = 'Gruppi utente che possono inviare un messaggio nella chat (solo utenti connessi ad un account).';
+$l['setting_dvz_sb_groups_recall'] = 'Permessi gruppo: Scrolla indietro per vedere i messaggi precedenti';
+$l['setting_dvz_sb_groups_recall_desc'] = 'Gruppi utente che possono visualizzare i messaggi precedenti scrollando indietro.';
+$l['setting_dvz_sb_groups_mod'] = 'Group permissions: Moderazione';
+$l['setting_dvz_sb_groups_mod_desc'] = 'Gruppi utente che possono moderare la chat (modificare e eliminare i messaggi).';
+$l['setting_dvz_sb_groups_mod_own'] = 'Permessi gruppo: Moderazione dei propri messaggi';
+$l['setting_dvz_sb_groups_mod_own_desc'] = 'Gruppi utente i cui membri possono modificare o eliminare i propri messaggi.';
+$l['setting_dvz_sb_supermods'] = 'I super moderatori sono moderatori della chat';
+$l['setting_dvz_sb_supermods_desc'] = 'Permette automaticamente ai super moderatori del forum di moderare la chat.';
+$l['setting_dvz_sb_blocked_users'] = 'Utenti bannati';
+$l['setting_dvz_sb_blocked_users_desc'] = 'Lista ID degli utenti bannati dalla chat separati da una virgola.';
---
+++ inc/languages/italiano/dvz_shoutbox.lang.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * MyBB Copyright 2016 MyBB Group, All Rights Reserved
+ *
+ * Website: http://www.mybb.com
+ * License: http://www.mybb.com/about/license
+ *
+ */
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+// server
+$l['dvz_sb_shoutbox'] = 'Chat';
+$l['dvz_sb_archive'] = 'Archivio chat';
+$l['dvz_sb_archivelink'] = 'archivio';
+$l['dvz_sb_last_read_link'] = 'Vai all\'ultimo letto';
+$l['dvz_sb_last_read_unmark_all'] = 'Segna tutti come letti';
+$l['dvz_sb_default'] = 'Inserisci il tuo messaggio...';
+$l['dvz_sb_user_blocked'] = 'Sei stato bloccato dall\'inviare messaggi nella chat.';
+$l['dvz_sb_minposts'] = 'Devi avere almeno {MINPOST} messaggi nel forum per poter usare la chat.';
+$l['dvz_sb_mod'] = 'Moderazione chat';
+$l['dvz_sb_mod_banlist'] = 'ID utenti bannati (separati da una virgola)';
+$l['dvz_sb_mod_banlist_button'] = 'Salva';
+$l['dvz_sb_mod_clear'] = 'Elimina i messaggi pi&ugrave; vecchi di...';
+$l['dvz_sb_mod_clear_all'] = 'Tutti i messaggi';
+$l['dvz_sb_mod_clear_button'] = 'Elimina';
+$l['dvz_sb_activity'] = 'Guardando l\'<a href="%s">archivio della chat</a>';
+
+// JavaScript
+$l['dvz_sb_delete_confirm'] = 'Vuoi davvero eliminare questo messaggio?';
+$l['dvz_sb_antiflood'] = 'Aspetta almeno {ANTIFLOOD} secondi prima di inviare un messaggio.';
+$l['dvz_sb_permissions'] = 'Non hai i permessi per effettuare questa azione.';
