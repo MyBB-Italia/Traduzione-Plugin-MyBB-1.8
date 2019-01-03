45d81be40bfaa463ad9f14fb22ffde65acd0dce8
chack1172 <NBerardozzi@gmail.com>
2019-01-03 16:07:19 +0100

    Aggiunto MyReactions
    Aggiunta la traduzione del plugin MyReactions alla raccolta

---
+++ inc/languages/italiano/admin/forum_myreactions.lang.php
@@ -0,0 +1,72 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: MyBB Italia
+// Plugin: MyReactions
+// --------------------------------------------------------------------------------
+
+/**
+ * MyReactions 0.0.2 - Admin Language File
+
+ * Copyright 2016 Matthew Rogowski
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ ** http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**/
+
+$l['myreactions'] = 'MyReactions';
+$l['can_manage_myreactions'] = 'Può gestire MyReactions?';
+
+$l['manage_reactions'] = "Gestiscisci MyReactions";
+$l['manage_reactions_desc'] = "Questa sezione ti permette di modificare ed eliminare le tue reazioni.";
+$l['add_reaction'] = "Aggiungi Nuova Reazione";
+$l['add_reaction_desc'] = "Qui puoi aggiungere una nuova singola reazione.";
+$l['add_multiple_reactions'] = "Aggiungi Reazioni Multiple";
+$l['add_multiple_reactions_desc'] = "Qui puoi aggiungere nuove reazioni in una sola volta.";
+$l['edit_reaction'] = "Modifica Reazione";
+$l['edit_reaction_desc'] = "Qui puoi modificare una singola reazione.";
+$l['mass_edit'] = "Modifica di Massa";
+$l['mass_edit_desc'] = "Qui puoi modificare facilmente tutte le tue reazioni in una sola volta.";
+
+$l['no_reactions'] = "Non ci sono reazione nel tuo forum in questo momento.";
+
+$l['image'] = "Immagine";
+$l['name'] = "Nome";
+$l['image_path'] = "Percorso Immagine";
+$l['image_path_desc'] = "Questo è il percorso all'immagine della reazione.";
+$l['include'] = "Aggiungere?";
+$l['path_to_images'] = "Percorso Immagini";
+$l['path_to_images_desc'] = "Questo è il percorso alla cartella che contiene le immagini.";
+$l['reaction_delete'] = "Eliminare?";
+$l['save_reaction'] = "Salva Reazione";
+$l['save_reactions'] = "Salva Reazioni";
+$l['show_reactions'] = "Mostra Reazioni";
+$l['reset'] = "Ripristina";
+
+$l['error_missing_name'] = "Non hai inserito un nome alla reazione.";
+$l['error_missing_path'] = "Non hai inserito un percorso alla reazione.";
+$l['error_missing_path_multiple'] = "Non hai inserito un percorso.";
+$l['error_no_reactions'] = "Non ci sono reazioni nella cartella specificata, o tutte le reazioni nella cartella sono già state aggiunte.";
+$l['error_no_images'] = "Non ci sono immagini nella cartella specificata.";
+$l['error_none_included'] = "Non hai selezionato alcuna reazione da includere.";
+$l['error_invalid_path'] = "Non hai inserito un percorso valido.";
+$l['error_invalid_reaction'] = "La reazione specificata non esiste.";
+
+$l['success_reaction_added'] = "La reazione è stata aggiunta con successo.";
+$l['success_multiple_reactions_added'] = "Le reazioni selezionate sono state aggiunte con successo.";
+$l['success_reaction_updated'] = "La reazione è stata aggiornata con successo.";
+$l['success_multiple_reactions_updated'] = "Le reazioni sono state aggiornate con successo.";
+$l['success_reaction_deleted'] = "La reazione selezionata è stata eliminata con successo.";
+$l['success_mass_edit_updated'] = "Le reazioni sono state aggiornate con successo.";
+
+$l['confirm_reaction_deletion'] = "Sei sicuro di voler eliminare questa reazione?";
