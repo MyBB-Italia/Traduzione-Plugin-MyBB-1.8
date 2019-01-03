aa2e7a969dad312767dd0b3ae5e858ae3897720b
chack1172 <NBerardozzi@gmail.com>
2019-01-03 16:01:43 +0100

    Aggiunto My Permissions
    Aggiunta la traduzione del plugin My Permissions alla raccolta

---
+++ inc/languages/italiano/admin/mypermissions.lang.php
@@ -0,0 +1,101 @@
+<?php
+/***************************************************************************
+ *
+ *  My Advertisements plugin (/inc/languages/english/admin/mypermissions.lang.php)
+ *  Author: Pirata Nervo
+ *  Copyright: © 2014 Pirata Nervo
+ *  
+ *  Website: http://mybb-plugins.com
+ *  License: license.txt
+ *
+ *  This plugin allows you to manage permissions on a larger scale.
+ *
+ ***************************************************************************/
+ 
+/****************************************************************************
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************/
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['mypermissions'] = 'My Permissions';
+$l['mypermissions_index'] = 'My Permissions';
+$l['mypermissions_canmanage'] = 'Vuoi gestire My Permissions?';
+
+// Tabs
+$l['mypermissions_rules'] = 'Regole';
+$l['mypermissions_rules_desc'] = 'Gestisci il file dei permessi.';
+$l['mypermissions_rules_add'] = 'Aggiungi';
+$l['mypermissions_rules_add_desc'] = 'Aggiungi una nuova regola ai permessi.';
+$l['mypermissions_rules_edit'] = 'Modifica';
+$l['mypermissions_rules_edit_desc'] = 'Modifica una regola esistente dei permessi.';
+$l['mypermissions_rules_delete'] = 'Elimina';
+$l['mypermissions_rules_delete_desc'] = 'Cancella una regola esistente dei permessi.';
+
+// General
+$l['mypermissions_edit'] = 'Modifica';
+$l['mypermissions_delete'] = 'Elimina';
+$l['mypermissions_submit'] = 'Invia';
+$l['mypermissions_reset'] = 'Ripristina';
+
+// Error messages
+$l['mypermissions_norules'] = 'Nessuna regola dei permessi trovata.';
+$l['mypermissions_invalid_rule'] = 'Regola dei permessi invalida.';
+$l['mypermissions_missing_field'] = 'Una o più campi sono mancanti.';
+$l['mypermissions_unknown_error'] = 'Si è riscontrato un\'errore sconosciuto.';
+
+// Table cat
+$l['mypermissions_file'] = 'File';
+$l['mypermissions_description'] = 'Descrizione';
+$l['mypermissions_usergroups'] = 'Gruppo Utenti';
+$l['mypermissions_field'] = 'Campo';
+$l['mypermissions_value'] = 'Valore';
+$l['mypermissions_options'] = 'Opzioni';
+
+// Rules - Add
+$l['mypermissions_addrule'] = 'Aggiungi regola';
+$l['mypermissions_addrule_file'] = 'File';
+$l['mypermissions_addrule_file_desc'] = 'Inserire il file in cui si desidera applicare i permessi.';
+$l['mypermissions_addrule_field'] = 'Campo';
+$l['mypermissions_addrule_field_desc'] = 'Inserire il nome del campo che vuoi bloccare. Può rimanere vuoto se vuoi bloccare ogni campo del file sopra specificato.';
+$l['mypermissions_addrule_value'] = 'Valore';
+$l['mypermissions_addrule_value_desc'] = 'Inserire il valore che deve corrispondere al campo sopra.';
+$l['mypermissions_addrule_description'] = 'Descrizione';
+$l['mypermissions_addrule_description_desc'] = 'Inserire una descrizione per questa regola.';
+$l['mypermissions_addrule_usergroups'] = 'Gruppo Utenti';
+$l['mypermissions_addrule_usergroups_desc'] = 'Selezione un gruppo utenti che vuoi bloccare.';
+$l['mypermissions_rule_added'] = 'Una nuova regola dei permessi è stata aggiunta correttamente.';
+
+// Rules - Edit
+$l['mypermissions_editrule'] = 'Modifica regole';
+$l['mypermissions_editrule_file'] = 'File';
+$l['mypermissions_editrule_file_desc'] = 'Inserisci il file in cui si desidera applicare i permessi.';
+$l['mypermissions_editrule_field'] = 'Campo';
+$l['mypermissions_editrule_field_desc'] = 'Inserire il nome del campo che vuoi bloccare. Può rimanere vuoto se vuoi bloccare ogni campo del file sopra specificato.';
+$l['mypermissions_editrule_value'] = 'Valore';
+$l['mypermissions_editrule_value_desc'] = 'Inserire il valore che deve corrispondere al campo sopra..';
+$l['mypermissions_editrule_description'] = 'Descrizione';
+$l['mypermissions_editrule_description_desc'] = 'Inserire una descrizione per questa regola.';
+$l['mypermissions_editrule_usergroups'] = 'Gruppo Utenti';
+$l['mypermissions_editrule_usergroups_desc'] = 'Selezione un gruppo utenti che vuoi bloccare.';
+$l['mypermissions_rule_edited'] = 'La regola selezionata è stata modificata correttamente.';
+
+// Rules - Delete
+$l['mypermissions_rule_deleted'] = 'La regola selezionata è stata cancellata correttamente.';
+$l['mypermissions_confirm_deleterule'] = 'Sei sicuro di volere eliminare la regola selezionata?';
