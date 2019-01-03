757d3701db050e34a4fdcc7f9121d49fcfb3f77e
chack1172 <NBerardozzi@gmail.com>
2019-01-03 13:16:32 +0100

    Aggiunto Amnesia
    Aggiunta la traduzione del plugin Amnesia alla raccolta

---
+++ inc/languages/italiano/amnesia.lang.php
@@ -0,0 +1,67 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['amnesia_privacy_policy_agreement_required'] = 'Prima di utilizzare il sito web leggi e accetta la Privacy Policy.';
+$l['amnesia_privacy_policy_signup_statement'] = 'Continuando accetti la <a href="{1}">Privacy Policy</a>.';
+$l['amnesia_privacy_policy_accept'] = 'Accetta';
+$l['amnesia_privacy_policy_date'] = 'Ultimo aggiornamento {1}';
+$l['amnesia_privacy_policy_accepted'] = 'Grazie, ora sarai portato al contenuto che stavi visualizzando.';
+
+$l['amnesia_personal_data_erasure'] = 'Cancellazione Dati Personali';
+$l['amnesia_personal_data_erasure_description'] = 'Qui puoi richiedere la cancellazione dei tuoi dati personali che comporterà anche la <strong>disattivazione permanente del tuo account</strong>. Un codice di verifica sarà inviato al tuo indirizzo email per confermare la richiesta.';
+$l['amnesia_personal_data_erasure_waiting_period_days'] = 'I dati saranno rimossi dopo {1} giorni, durante i quali potrai cancellare l\'operazione dopo aver eseguito l\'accesso.';
+$l['amnesia_personal_data_erasure_approval'] = 'I tuoi dati saranno eliminati non appena un amministratore approverà la tua richiesta.';
+$l['amnesia_personal_data_erasure_options'] = 'Opzioni per la cancellazione';
+$l['amnesia_personal_data_erasure_content'] = 'Rimuovi Contenuti Inviati';
+$l['amnesia_personal_data_erasure_content_description'] = 'Scegli se eliminare anche il contenuto inviato con il tuo account (messaggi, discussioni, etc.)';
+$l['amnesia_personal_data_erasure_comment'] = 'Commento';
+$l['amnesia_personal_data_erasure_comment_description'] = 'Puoi includere dettagli aggiuntivi relativi alla richiesta per l\'amministratore.';
+$l['amnesia_personal_data_erasure_proceed'] = 'Richiedi Cancellazione';
+$l['amnesia_personal_data_erasure_no_super_admin'] = 'Rimuovi il tuo account dalla lista dei super amministratori per richiedere la cancellazione dei dati.';
+$l['amnesia_personal_data_erasure_requested'] = 'Un codice di verifica è stato inviato al tuo indirizzo email.';
+$l['amnesia_personal_data_erasure_request_verification_subject'] = 'Conferma la richiesta di cancellazione dei tuoi dati personali';
+$l['amnesia_personal_data_erasure_request_verification_message'] = '{1},
+
+Per completare il processo di cancellazione dei dati personali su {2}, devi andare all\'indirizzo URL qui sotto in un browser web. Il codice è valido per 24 ore.
+
+{3}
+
+Grazie,
+Lo Staff di {2}';
+$l['amnesia_personal_data_erasure_pending'] = 'Attesa cancellazione dati personali';
+$l['amnesia_personal_data_erasure_pending_message'] = 'Hai richiesto la cancellazione dei dati personali';
+$l['amnesia_personal_data_erasure_scheduled_for'] = 'La cancellazione è prevista per il {1}.';
+$l['amnesia_personal_data_erasure_cancel'] = 'Annulla Cancellazione';
+
+$l['amnesia_personal_data_erasure_awaiting_approval_single'] = 'C\'è una richiesta di cancellazione dei dati personali in attesa di approvazione. Vai al tuo ACP per visualizzare la richiesta.';
+$l['amnesia_personal_data_erasure_awaiting_approval_plural'] = 'Ci sono {1} richieste di cancellazione dei dati personali in attesa di approvazione. Vai al tuo ACP per visualizzare le richieste.';
+$l['amnesia_personal_data_erasure_awaiting_approval_link'] = ' <a href="{1}">Vai al Pannello Amministrazione</a>.';
+
+
+$l['amnesia_personal_data_export'] = 'Esportazione Dati Personali';
+$l['amnesia_personal_data_export_description'] = 'Qui puoi richiedere una copia dei tuoi dati personali in un formato XML leggibile a macchina. Un codice di verifica sarà inviato al tuo indirizzo email per confermare la richiesta.';
+$l['amnesia_personal_data_export_key_invalid'] = 'Il link di verifica deve essere usato con lo stesso dispositivo e con la stessa sessione di navigazione. Richiedi una nuova richiesta di esportazione.';
+$l['amnesia_personal_data_export_proceed'] = 'Continua';
+$l['amnesia_personal_data_export_requested'] = 'Un codice di verifica è stato inviato al tuo indirizzo email.';
+$l['amnesia_personal_data_export_request_verification_subject'] = 'Conferma la richiesta di esportazione dei tuoi dati';
+$l['amnesia_personal_data_export_request_verification_message'] = '{1},
+
+Per completare il processo di esportazione dei dati personali su {2}, devi andare all\'indirizzo URL qui sotto nel browser web che hai utilizzato per inviare la richiesta. Il codice è valido per 24 ore.
+
+{3}
+
+Grazie,
+Lo Staff di {2}';
+$l['amnesia_personal_data_export_verify'] = 'Ora puoi scaricare un file con le informazioni personali relative al tuo account.';
+$l['amnesia_personal_data_export_download'] = 'Scarica';
+
+$l['amnesia_verification_code_invalid'] = 'Il codice di verifica non è valido o è scaduto.';
+
+$l['amnesia_anonymized_username_prefix'] = 'Utente ';
+
+$l['amnesia_erasure_task_ran'] = 'L\'operazione di cancellazione di Amnesia è stata eseguita con successo.';
