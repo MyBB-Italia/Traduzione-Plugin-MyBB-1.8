bc2d889b132a0aeccd128edcba981ac1ed2258f4
chack1172 <NBerardozzi@gmail.com>
2019-01-03 13:16:32 +0100

    Aggiunto Amnesia
    Aggiunta la traduzione del plugin Amnesia alla raccolta

---
+++ inc/languages/italiano/admin/amnesia.lang.php
@@ -0,0 +1,36 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['amnesia_admin_pluginlibrary_missing'] = 'Aggiungi <a href="https://github.com/frostschutz/MyBB-PluginLibrary">PluginLibrary</a> per porte utilizzare il plugin';
+
+$l['amnesia_admin_personal_data_erasure_requests'] = 'Richieste Cancellazione Dati';
+$l['amnesia_admin_personal_data_erasure_requests_unapproved'] = 'Richieste Non Approvate';
+$l['amnesia_admin_personal_data_erasure_requests_unapproved_description'] = 'Questa sezione ti permette di gestire le richieste di cancellazione dei dati personali.';
+
+$l['amnesia_admin_personal_data_erasure_requests_completed'] = 'Richieste Completate';
+$l['amnesia_admin_personal_data_erasure_requests_completed_description'] = 'Questa sezione ti permette di vedere le richieste di cancellazione completate.';
+
+$l['amnesia_admin_user'] = 'Utente';
+$l['amnesia_admin_user_id'] = 'ID {1}';
+$l['amnesia_admin_date'] = 'Data';
+$l['amnesia_admin_scheduled_date'] = 'Programmato per';
+$l['amnesia_admin_action_date'] = 'Cancellato';
+$l['amnesia_admin_with_content'] = 'Contenuto';
+$l['amnesia_admin_comment'] = 'Commento';
+$l['amnesia_admin_approve'] = 'Approva';
+$l['amnesia_admin_cancel'] = 'Cancella';
+$l['amnesia_admin_personal_data_erasure_requests_unapproved_empty'] = 'Nessuna richiesta in attesa di approvazione.';
+$l['amnesia_admin_personal_data_erasure_requests_completed_empty'] = 'Nessuna richiesta completata.';
+$l['amnesia_admin_personal_data_erasure_approve_confirm_title'] = 'Conferma richiesta di approvazione';
+$l['amnesia_admin_personal_data_erasure_approve_confirm_message'] = 'Sei sicuro di voler approvare la richiesta di cancellazione selezionata?';
+$l['amnesia_admin_personal_data_erasure_cancel_confirm_title'] = 'Conferma richiesta di cancellazione';
+$l['amnesia_admin_personal_data_erasure_cancel_confirm_message'] = 'Sei sicuro di voler cancellare la richiesta di cancellazione selezionata?';
+$l['amnesia_admin_personal_data_erasure_request_approved'] = 'La richiesta selezionata è stata approvata.';
+$l['amnesia_admin_personal_data_erasure_request_canceled'] = 'La richiesta selezionata è stata cancellata.';
+
+$l['amnesia_erasure_task_ran'] = 'L\'operazione di cancellazione di Amnesia è stata eseguita con successo.';
