f3dfb9dfc8ef9a6127312329bb336e52554ab131
chack1172 <NBerardozzi@gmail.com>
2019-01-03 15:50:56 +0100

    Aggiunto MyAlerts
    Aggiunta la traduzione del plugin MyAlerts alla raccolta

---
+++ inc/languages/italiano/admin/myalerts.lang.php
@@ -0,0 +1,39 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['myalerts'] = "MyAlerts";
+$l['myalerts_pluginlibrary_missing'] = "Il plugin selezionato non può essere installato poichè <a href=\"http://mods.mybb.com/view/pluginlibrary\">PluginLibrary</a> è mancante.";
+
+$l['setting_group_myalerts'] = "Impostazioni MyAlerts";
+$l['setting_group_myalerts_desc'] = "Impostazioni per il plugin MyAlerts";
+$l['setting_myalerts_perpage'] = "Avvisi per pagina";
+$l['setting_myalerts_perpage_desc'] = "Quanti avvisi desideri mostrare nella lista delle pagine degli avvisi? (di default è 10)";
+$l['setting_myalerts_dropdown_limit'] = "Numero di avvisi da mostrate nel menù a tendina";
+$l['setting_myalerts_dropdown_limit_desc'] = "Quanti avvisi desideri mostrate nel menù a tendina globale? (di default è 10)";
+$l['setting_myalerts_autorefresh'] = "Pagina AJAX MyAlerts autorefresh";
+$l['setting_myalerts_autorefresh_desc'] = "Quanto spesso (in secondi) desideri che le pagine MyALerts ricarichino gli avvisi con AJAX? (0 se non vuoi l'autorefresh)";
+$l['setting_myalerts_avatar_size'] = "Dimensioni dell'avatar";
+$l['setting_myalerts_default_avatar_desc'] = "Le dimensioni da usare quando mostri gli avatar nella lista degli avvisi. Nella forma x|y. Esempio: 64|64.";
+
+$l['myalerts_task_cleanup_ran'] = 'Gli avvisi letti da oltre una settimana sono stati cancellati correttamente!';
+$l['myalerts_task_cleanup_error'] = 'Qualcosa è andato storto quando mentre pulivo gli avvisi...';
+
+$l['myalerts_task_title'] = 'Pulizia MyAlerts';
+$l['myalerts_task_description'] = 'Un servizio di pulizia per i vecchi avvisi già letti. Questa opzione è richiesta, altrimenti la tabella degli avvisi potrebbe raggiungere dimensioni enormi.';
+
+$l['myalerts_alert_types'] = 'Tipo di avvisi';
+$l['myalerts_can_manage_alert_types'] = 'Vuoi gestire i tipi di avvisi?';
+
+$l['myalerts_alert_type_code'] = 'Codice';
+$l['myalerts_alert_type_enabled'] = 'Abilitare?';
+$l['myalerts_alert_type_can_be_user_disabled'] = 'Può essere disabilitato dagli utenti?';
+$l['myalerts_no_alert_types'] = 'Nessuno tipo di avviso trovato!';
+$l['myalerts_update_alert_types'] = 'Aggiorna il tipo di avvisi';
+$l['myalerts_alert_types_updated'] = 'Tipi di avvisi aggiornati!';
+
+$l['myalerts_upgraded'] = 'MyAlerts è stato aggiornato. Tutte le vecchie impostazioni degli avvisi degli utenti andranno persi - assicurati di avvisare i tuoi utenti!';
---
+++ inc/languages/italiano/myalerts.lang.php
@@ -0,0 +1,54 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Modificata da: Massy
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['myalerts'] = 'MyAlerts';
+
+$l['myalerts_page_title'] = 'Avvisi recenti';
+$l['myalerts_page_getnew'] = 'Controlla i nuovi avvisi';
+$l['myalerts_alerts'] = 'Avvisi';
+
+$l['myalerts_settings_page_title'] = 'Impostazioni Avvisi';
+
+$l['myalerts_online_location_listing'] = 'Sta visualizzando gli avvisi';
+$l['myalerts_unread_title'] = 'Avvisi non letti';
+$l['myalerts_no_alerts'] = 'Non ci sono avvisi da mostrare';
+$l['myalerts_rep'] = '{1} ha modificato la tua reputazione.';
+$l['myalerts_pm'] = '{1} ti ha inviato un nuovo messaggio privato intitolato "{2}".';
+$l['myalerts_buddylist'] = '{1} ti ha aggiunto alla sua lista amici.';
+$l['myalerts_quoted'] = '{1} ti ha citato in {2}.';
+$l['myalerts_post_threadauthor'] = '{1} ha risposto al tuo argomento "{2}". Ci possono essere altri messagi dopo questo.';
+$l['myalerts_subscribed_thread'] = '{1} ha risposto alla tua sottoscrizione all\'argomento "{2}".';
+$l['myalerts_rated_threadauthor'] = '{1} ha votato la tua discussione <b>"{2}"</b>.';
+$l['myalerts_voted_threadauthor'] = '{1} ha votato nel tuo sondaggio <b>"{2}"</b>.';
+$l['myalerts_setting_rep'] = 'Ricevere avviso per la reputazione?';
+$l['myalerts_setting_pm'] = 'Ricevere avviso per i Messaggi Privati (PM)?';
+$l['myalerts_setting_buddylist'] = 'Ricevere avviso quando vieni aggiunto alla lista amici?';
+$l['myalerts_setting_quoted'] = 'Ricevere avviso quando sei quotato in un messaggio?';
+$l['myalerts_setting_post_threadauthor'] = 'Ricevere avviso quando qualcuno risponde ad una tua discussione?';
+$l['myalerts_setting_subscribed_thread'] = 'Ricevere avviso quando qualcuno risponde ad una discussione sottoscritta?';
+$l['myalerts_setting_rated_threadauthor'] = 'Ricevere avviso quando qualcuno vota la tua discussione?';
+$l['myalerts_setting_voted_threadauthor'] = 'Ricevere avviso quando qualcuno vota in un tuo sondaggio?';
+$l['myalerts_settings_save'] = 'Salva le impostazioni';
+$l['myalerts_settings_updated'] = 'Impostazioni degli avvisi aggiornata con successo. Rendirizzamento alla pagina delle impostazioni.';
+$l['myalerts_settings_updated_title'] = 'Impostazione degli Avvisi aggiornata.';
+
+$l['myalerts_delete_deleted'] = 'Avviso eliminato correttamente.';
+$l['myalerts_delete_error'] = 'L\'avviso non può essere cancellato al momento.';
+$l['myalerts_delete_read_confirm'] = 'Vuoi veramente eliminare tutti gli avvisi letti? Questa operazione non può essere annullata!';
+$l['myalerts_delete_all_confirm'] = 'Vuoi veramente eliminare tutti gli avvisi? Questa operazione non può essere annullata!';
+$l['myalerts_delete_mass_deleted'] = 'Avvisi Eliminati!';
+$l['myalerts_delete_all'] = 'Tutti gli avvisi sono stati eliminati correttamente.';
+
+$l['myalerts_usercp_nav'] = 'Avvisi';
+$l['myalerts_usercp_nav_alerts'] = 'Mostra Avvisi';
+$l['myalerts_usercp_nav_settings'] = 'Impostazione Avvisi';
+$l['myalerts_usercp_nav_delete_read'] = 'Elimina i Vecchi Avvisi';
+$l['myalerts_usercp_nav_delete_all'] = 'Elimina Tutti gli Avvisi';
+$l['myalerts_error_alert_not_found'] = 'Avviso non trovato.';
+$l['myalerts_delete'] = 'Elimina';
