bbae1aceae79ce15957dbab23ca0a80d0ffd2e3f
chack1172 <NBerardozzi@gmail.com>
2019-01-03 16:17:07 +0100

    Aggiunto Colored Usernames
    Aggiunta la traduzione del plugin Colored Usernames alla raccolta

---
+++ inc/languages/italiano/admin/colored_usernames.lang.php
@@ -0,0 +1,21 @@
+<?php
+// Language file (english) for MyBB Plugin Colored Usernames
+// © 2014 juventiner
+// ---------------------------------------------------
+// Last Update: 03.09.2014
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+$l['colored_usernames_info_name'] = 'Username colorati';
+$l['colored_usernames_info_desc'] = 'Questo plugin mostra gli username colorati in forumdisplay.php.';
+
+$l['colored_usernames_settings_name'] = 'Impostazioni di username colorati';
+$l['colored_usernames_settings_desc'] = 'Queste sono le impostazioni per il plugin degli Username Colorati.';
+
+$l['colored_usernames_settings_status_name'] = 'abilita gli Username Colorati';
+$l['colored_usernames_settings_status_desc'] = 'Per favore abilita il Plugin degli Username Colorati per mostrare gli username nel forumdisplay.php colorati.';
