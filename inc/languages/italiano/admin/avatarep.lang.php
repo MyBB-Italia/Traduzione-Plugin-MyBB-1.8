572bbda325eca61ab5aedf93d857968baf7382b7
chack1172 <NBerardozzi@gmail.com>
2019-01-03 13:25:37 +0100

    Aggiunto Last Poster Avatar
    Aggiunta la traduzione del plugin Last Poster Avatar alla raccolta

---
+++ inc/languages/italiano/admin/avatarep.lang.php
@@ -0,0 +1,67 @@
+<?php
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Modificata da: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+
+// Non rimuovere questi valori
+$l['avatarep_name'] = "Mostra l' Avatar nella lista delle discussioni e della lista forum";
+$l['avatarep_descrip'] = "Mostra l'avatar dell'utente dell'ultimo messaggio nella lista delle discussioni e dei forum, e anche i creatori delle discussioni nella lista delle discussioni.";
+$l['avatarep_config'] = "Configurazioni del plugin";
+$l['avatarep_style_usernames'] = "Plugin Style Usernames attivo (Disattiva l'impostazione Nomi utente formattati nelle impostazioni dell'avatar, o disinstalla Style Usernames dal forum per evitare errori.)";
+$l['avatarep_disabled_msg'] = "Plugin disattivato";
+
+// Frasi delle impostazioni del plugin
+$l['avatarep_title'] = "[Plugin] Mostra l'avatar nelle discussioni e nei forum";
+$l['avatarep_title_descrip'] = "Configura questo plugin";
+
+$l['avatarep_power'] = "Abilita/Disabilita il Plugin";
+$l['avatarep_power_descrip'] = "Selezionare se si desidera attivare questo plugin";
+
+$l['avatarep_forum'] = "Avatar nei Forum";
+$l['avatarep_forum_descrip'] = "Mostra l'avatar dell'utente nella lista dei forum";
+
+$l['avatarep_thread_owner'] = "Avatar nelle Discussioni";
+$l['avatarep_thread_owner_descrip'] = "Mostra l'avatar del creatore della discussione";
+
+$l['avatarep_thread_lastposter'] = "Avatar nelle Discussioni";
+$l['avatarep_thread_lastposter_descrip'] = "Mostra l'avatar dell'ultimo utente che ha risposta alla discussione";
+
+$l['avatarep_thread_contributor'] = "Mostra Avatar Contributore";
+$l['avatarep_thread_contributor_descrip'] = "Mostra l'avatar del creatore della discussione nella pagina visualizza discussione, o il tuo se hai risposto a tale discussione";
+
+$l['avatarep_thread_announcements'] = "Avatar negli Annunci";
+$l['avatarep_thread_announcements_descrip'] = "Mostra l'avatar dell'utente che ha creato l'annuncio";
+
+$l['avatarep_portal'] = "Avatar nel Portale";
+$l['avatarep_portal_descrip'] = "Mostra gli avatar degli ultimi messaggi e degli annunci nel portale";
+
+$l['avatarep_search'] = "Avatar nella Ricerca";
+$l['avatarep_search_descrip'] = "Mostra gli avatar nei risultati delle ricerche";
+
+$l['avatarep_menu'] = "Menu Popup";
+$l['avatarep_menu_descrip'] = "Mostra un menu con i dati dell'utente quando si clicca sul suo avatar";
+
+$l['avatarep_menu_events'] = "Evento del mouse";
+$l['avatarep_menu_events_descrip'] = "Seleziona l'evento del mouse con cui far apparire il menu.";
+
+$l['avatarep_guests'] = "Avatar Visitatori";
+$l['avatarep_guests_descrip'] = "Mostra l'avatar predefinito ai visitatori";
+
+$l['avatarep_format'] = "Nomi utente formattati";
+$l['avatarep_format_descrip'] = "Mostra i nomi formattati con il codice html configurato nel gruppo utente";
+
+$l['avatarep_private'] = "Avatar nei Messaggi Privati";
+$l['avatarep_private_descrip'] = "Mostra l'avatar nei messaggi privati";
+
+$l['avatarep_latest_threads'] = "Avatar nelle Ultime Discussioni";
+$l['avatarep_latest_threads_descrip'] = "Mostra l'avatar delle ultime discussioni nel Portale e nella index se è presente il plugin SideBoxes";
+
+$l['avatarep_thread_lastposter_mark'] = "Segna discussioni risposte";
+$l['avatarep_thread_lastposter_mark_descrip'] = "Mostra l'avatar dell'utente sopra l'avatar del creatore se lui ha risposto alla discussione, (richiede query aggiuntive).";
+
+$l['avatarep_onerror'] = "Mostra avatar bianchi o rotti come avatar di default";
+$l['avatarep_private_descrip'] = "Se si verifica un errore durante il caricamento dell'avatar, come l'immagine non è disponibile, o non viene caricata correttamente o semplicemente il server in cui si trova non risponde, viene caricato l'avatar di default (Questo rompe la validazione w3c perché la funzione onerror del tag img non è valida, ma esiste. se non vuoi che accada disattiva questa opzione).";
