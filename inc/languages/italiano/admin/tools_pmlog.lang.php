5e5523d8b1f89499c29331a30b837ae1f7c00b84
chack1172 <NBerardozzi@gmail.com>
2019-01-03 20:58:57 +0100

    Aggiunto Private Message Log
    Aggiunta la traduzione del plugin Private Message Log alla raccolta

---
+++ inc/languages/italiano/admin/tools_pmlog.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Private Message Log
+ * Copyright 2012 Starpaul20
+ */
+
+// --------------------------------------------------------------------------------
+// MyBB Traduzione Italiana
+// ================================================================================
+// Autore: redfuegolight
+// Modificata da: chack1172
+// Editore: MyBB Italia
+// --------------------------------------------------------------------------------
+ 
+$l['pmlog_info_name'] = "Log Messaggi Privati";
+$l['pmlog_info_desc'] = "Consente agli amministratori di visualizzare i messaggi privati inviati nella board.";
+
+// Front end logging warning
+$l['private_message_logging'] = "Tutti i messaggi inviati tramite questo modulo verranno registrati e resi visibili agli amministratori della board. Non includere nessuna informazione privata, finanziaria o password in tutti messaggio inviato.";
+
+// Admin Log logging
+$l['admin_log_tools_pmlog_view'] = "#{1} ha visto il PM inviato dall'utente #{3} ({2})";
+$l['admin_log_tools_pmlog_prune'] = "Eliminati {6} messaggi privati vecchi più di {1} giorni";
+$l['admin_log_tools_pmlog_prune_from'] = "Eliminati {6} messaggi privati vecchi più di {1} giorni inviati dall'utente #{2}";
+$l['admin_log_tools_pmlog_prune_to'] = "Eliminati {6} messaggi privati vecchi più di {1} giorni ricevuti dall'utente #{3}";
+$l['admin_log_tools_pmlog_prune_from_to'] = "Eliminati {6} messaggi privati vecchi più di {1} giorni inviati dall'utente #{2} e ricevuti dall'utente #{3}";
+$l['admin_log_tools_pmlog_delete'] = "Eliminati {1} messaggi privati";
+
+// Admin Permissions
+$l['can_manage_pm_logs'] = "Può gestire i registri dei messaggi privati?";
+
+// PM Log
+$l['private_message_log'] = "Log messaggi privati";
+$l['private_message_log_desc'] = "Qui è possibile visualizzare e cercare tutti i messaggi privati inviati.";
+$l['prune_private_messages'] = "Elimina Messaggi Privati";
+$l['prune_private_messages_desc'] = "Qui puoi eliminare i messaggi privati che corrispondono a criteri specifici.";
+
+$l['all_users'] = "Tutti gli Utente";
+$l['from_user'] = "Dall'Utente:";
+$l['to_user'] = "All'Utente:";
+$l['date_range'] = "Intervallo Date:";
+$l['older_than'] = "Più vecchi di ";
+$l['days'] = "Giorni";
+$l['in_folder'] = "Nella Cartella:";
+$l['read_status'] = "Stato Lettura:";
+$l['unread_only'] = "Soltanto non letti";
+$l['read_only'] = "Soltanto letti";
+$l['both'] = "Entrambi";
+
+$l['subject'] = "Oggetto";
+$l['from'] = "Da";
+$l['to'] = "A";
+$l['date_sent'] = "Data Inviato";
+$l['ip_address'] = "Indirizzo IP";
+
+$l['find_pms_by_user'] = "Trova tutti i messaggi privati inviati da questo utente";
+$l['find_pms_to_user'] = "Trova tutti i messaggi privati inviati a questo utente";
+$l['find'] = "Trova";
+$l['new_pm'] = "Nuovo PM";
+$l['old_pm'] = "Vecchio PM";
+$l['reply_pm'] = "Replica PM";
+$l['fwd_pm'] = "Inoltro PM";
+
+$l['delete_selected'] = "Elimina Selezionati";
+$l['confirm_delete_pms'] = "Eliminare i messaggi privati selezionati?";
+$l['success_pruned_private_messages'] = "I messaggi privati selezionati sono stati eliminati.";
+
+$l['filter_private_message_log'] = "Filtra Log Messaggi Privati";
+$l['subject_contains'] = "Oggetto";
+$l['folder'] = "Cartella";
+$l['inbox'] = "Posta in arrivo";
+$l['sent_items'] = "Oggetti inviati";
+$l['drafts'] = "Bozze";
+$l['trash_can'] = "Cestino";
+$l['other'] = "Altro";
+
+$l['no_logs'] = "Non è presente alcuna voce nel log con i criteri selezionati.";
+$l['error_invalid_user'] = "L'utente specificato non è valido.";
+
+$l['private_message_log_viewer'] = "Visualizzatore log dei messaggi privati";
+$l['close_window'] = "Chiudi la finestra";
+$l['from'] = "Da";
+$l['to'] = "A";
+$l['subject'] = "Oggetto";
+$l['date'] = "Data";
+$l['private_message'] = "Messaggio privato";
+
+$l['na_deleted'] = "N/A - Eliminato";
+$l['search_for_a_user'] = "Cerca un utente"; // Needed to matain compatibility with pre-1.8.5 versions of MyBB
